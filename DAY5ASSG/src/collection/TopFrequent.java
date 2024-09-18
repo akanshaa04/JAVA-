da2db955d36a669b2e7ee2333d0c5c9bf91c369f
akanshaa04 <akanshaa.incedo08@gmail.com>
2024-09-18 09:53:48 +0530

    Update TopFrequent.java

--- DAY5ASSG/src/collection/TopFrequent.java
+++ DAY5ASSG/src/collection/TopFrequent.java
@@ -1,15 +1,20 @@
 package collection;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class TopFrequent {
 
 	public static void main(String st[]) {
 
-		ArrayList<String> list = new ArrayList<>(15);
-
+		List<String> list = new ArrayList<>(15);
+////		List<String> ls = Arrays.asList("a","v");
+////		aise bhi kar skte hai initialize
+//
 		list.add("Apple");
 		list.add("Cucumber");
 		list.add("Banana");
@@ -37,6 +42,18 @@ public class TopFrequent {
 			else {
 				mp.put(it, 1);
 			}
+		}
+		
+		
+		
+		List<Entry<String,Integer>> li = new  ArrayList<Entry<String,Integer>>();
+		
+		li.addAll(mp.entrySet());
+		
+		Collections.sort(li, (x,y)-> y.getValue().compareTo(x.getValue()));
+		
+		for(int i =0 ; i < 3 ; i++) {
+			System.out.println(li.get(i));
 		}
 		
 		
